d654466a29f449157826e7e8a52a2229992f819c
code99-ash <oyetikh1@gmail.com>
2023-07-07 10:54:14 +0100

    updated docker-compose

--- authserver/config/mongoose.js
+++ authserver/config/mongoose.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose')
 
+// const connectMongoose = async () => {
+//     try {
+//         await mongoose.connect(
+//             process.env.DB_CONNECT, 
+//             {useNewUrlParser: true,useUnifiedTopology: true},
+//         );
+//         console.log('Connected to db')
+//     } catch (error) {
+//         console.log('error connecting to db', error)
+//     }
+// }
+
 const connectMongoose = async () => {
     try {
-        await mongoose.connect(
-            process.env.DB_CONNECT, 
-            {useNewUrlParser: true,useUnifiedTopology: true},
-        );
+        await mongoose.connect('mongodb://mongodb:27017/storems', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
         console.log('Connected to db')
     } catch (error) {
         console.log('error connecting to db', error)
     }
 }
+
 
 module.exports = connectMongoose
